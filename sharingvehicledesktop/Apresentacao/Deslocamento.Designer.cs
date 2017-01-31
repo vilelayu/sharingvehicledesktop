81f628fba43bec3a0dd4e7ca32cf44742ec3211b
PedroMarcus99 <pedromarcussilva38@gmail.com>
2017-01-31 01:11:07 -0200

    Formulário de Deslocamento feito.

---
+++ sharingvehicledesktop/Apresentacao/Deslocamento.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace sharingvehicledesktop.Apresentacao
+{
+    partial class Deslocamento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIDdeslocamento = new System.Windows.Forms.TextBox();
+            this.txtIdCarroDeslocamento = new System.Windows.Forms.TextBox();
+            this.txtIdMotoristaDeslocamento = new System.Windows.Forms.TextBox();
+            this.txtDataDeslocamento = new System.Windows.Forms.TextBox();
+            this.txtQuilometragemDeslocamento = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnFecharTela = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btanAtualizarDeslocamento = new System.Windows.Forms.Button();
+            this.btnSalarDeslocamento = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIDdeslocamento
+            // 
+            this.txtIDdeslocamento.Location = new System.Drawing.Point(194, 78);
+            this.txtIDdeslocamento.Name = "txtIDdeslocamento";
+            this.txtIDdeslocamento.Size = new System.Drawing.Size(67, 20);
+            this.txtIDdeslocamento.TabIndex = 0;
+            // 
+            // txtIdCarroDeslocamento
+            // 
+            this.txtIdCarroDeslocamento.Location = new System.Drawing.Point(194, 104);
+            this.txtIdCarroDeslocamento.Name = "txtIdCarroDeslocamento";
+            this.txtIdCarroDeslocamento.Size = new System.Drawing.Size(67, 20);
+            this.txtIdCarroDeslocamento.TabIndex = 1;
+            // 
+            // txtIdMotoristaDeslocamento
+            // 
+            this.txtIdMotoristaDeslocamento.Location = new System.Drawing.Point(194, 130);
+            this.txtIdMotoristaDeslocamento.Name = "txtIdMotoristaDeslocamento";
+            this.txtIdMotoristaDeslocamento.Size = new System.Drawing.Size(67, 20);
+            this.txtIdMotoristaDeslocamento.TabIndex = 2;
+            // 
+            // txtDataDeslocamento
+            // 
+            this.txtDataDeslocamento.Location = new System.Drawing.Point(194, 156);
+            this.txtDataDeslocamento.Name = "txtDataDeslocamento";
+            this.txtDataDeslocamento.Size = new System.Drawing.Size(67, 20);
+            this.txtDataDeslocamento.TabIndex = 3;
+            // 
+            // txtQuilometragemDeslocamento
+            // 
+            this.txtQuilometragemDeslocamento.Location = new System.Drawing.Point(194, 182);
+            this.txtQuilometragemDeslocamento.Name = "txtQuilometragemDeslocamento";
+            this.txtQuilometragemDeslocamento.Size = new System.Drawing.Size(67, 20);
+            this.txtQuilometragemDeslocamento.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(172, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "DATA";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "QUILOMETRAGEM DESLOCADA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(125, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "CARRO - ID ";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(99, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "MOTORISTA - ID ";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // btnFecharTela
+            // 
+            this.btnFecharTela.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFecharTela.Location = new System.Drawing.Point(438, 12);
+            this.btnFecharTela.Name = "btnFecharTela";
+            this.btnFecharTela.Size = new System.Drawing.Size(75, 23);
+            this.btnFecharTela.TabIndex = 13;
+            this.btnFecharTela.Text = "Fechar Tela ";
+            this.btnFecharTela.UseVisualStyleBackColor = true;
+            this.btnFecharTela.Click += new System.EventHandler(this.btnFecharTela_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLimpar.Location = new System.Drawing.Point(276, 330);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 14;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // btanAtualizarDeslocamento
+            // 
+            this.btanAtualizarDeslocamento.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btanAtualizarDeslocamento.Location = new System.Drawing.Point(357, 330);
+            this.btanAtualizarDeslocamento.Name = "btanAtualizarDeslocamento";
+            this.btanAtualizarDeslocamento.Size = new System.Drawing.Size(75, 23);
+            this.btanAtualizarDeslocamento.TabIndex = 15;
+            this.btanAtualizarDeslocamento.Text = "Atualizar";
+            this.btanAtualizarDeslocamento.UseVisualStyleBackColor = true;
+            // 
+            // btnSalarDeslocamento
+            // 
+            this.btnSalarDeslocamento.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSalarDeslocamento.Location = new System.Drawing.Point(438, 330);
+            this.btnSalarDeslocamento.Name = "btnSalarDeslocamento";
+            this.btnSalarDeslocamento.Size = new System.Drawing.Size(75, 23);
+            this.btnSalarDeslocamento.TabIndex = 16;
+            this.btnSalarDeslocamento.Text = "Salvar";
+            this.btnSalarDeslocamento.UseVisualStyleBackColor = true;
+            // 
+            // Deslocamento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(525, 370);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnSalarDeslocamento);
+            this.Controls.Add(this.btanAtualizarDeslocamento);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnFecharTela);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtQuilometragemDeslocamento);
+            this.Controls.Add(this.txtDataDeslocamento);
+            this.Controls.Add(this.txtIdMotoristaDeslocamento);
+            this.Controls.Add(this.txtIdCarroDeslocamento);
+            this.Controls.Add(this.txtIDdeslocamento);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.MaximizeBox = false;
+            this.Name = "Deslocamento";
+            this.Text = "Deslocamento";
+            this.Load += new System.EventHandler(this.Deslocamento_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIDdeslocamento;
+        private System.Windows.Forms.TextBox txtIdCarroDeslocamento;
+        private System.Windows.Forms.TextBox txtIdMotoristaDeslocamento;
+        private System.Windows.Forms.TextBox txtDataDeslocamento;
+        private System.Windows.Forms.TextBox txtQuilometragemDeslocamento;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnFecharTela;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btanAtualizarDeslocamento;
+        private System.Windows.Forms.Button btnSalarDeslocamento;
+    }
+}
